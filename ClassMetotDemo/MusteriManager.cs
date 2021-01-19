8356c000e5dc50d9091fe8d3d7c6581c0c343f1d
Abdullah ÇETİN <cetin.ab@outlook.com>
2021-01-19 21:04:10 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri[] musteriEkle)
+        {
+            foreach (Musteri musteri1 in musteriEkle)
+            {
+                Console.WriteLine("{0}: isimli yeni müşteri takip listesine başarıyla eklendi", musteri1.FirstName);
+                Console.WriteLine("-------------------------------------------------------------------------------");
+            }
+        }
+        public void MusterileriGoster(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Müşterinin Adı    : {0} \nMüşterinin Soyadı : {1} \nMüşterinin Maaşı  : {2}", musteri.FirstName, musteri.LastName, musteri.Salary);
+                Console.WriteLine("---------------------------------------------");
+            }
+        }
+
+        public void Sil(Musteri musteriSil)
+        {
+            Console.WriteLine("{0} : isimli müşteri takip listesinden çıkarılmıştır", musteriSil.FirstName);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.FirstName = "Engin";
+            musteri1.LastName = "DEMİROĞ";
+            musteri1.Salary = 30000;
+            Musteri musteri2 = new Musteri();
+            musteri2.FirstName = "Abdullah";
+            musteri2.LastName = "ÇETİN";
+            musteri2.Salary = 3000;
+            Musteri musteri3 = new Musteri();
+            musteri3.FirstName = "Yasin";
+            musteri3.LastName = "ÇETİN";
+            musteri3.Salary = 2500;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriTakip = new MusteriManager();
+
+            musteriTakip.Ekle(musteriler);
+            musteriTakip.MusterileriGoster(musteriler);
+            musteriTakip.Sil(musteri3);
+            Console.ReadLine();
+        }
+    }
+}
